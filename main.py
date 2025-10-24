78d10d00a9c3ffc3ae34a6120d7e5aebb742f4a6
CryptEAX <crypteax@proton.me>
2025-10-24 15:49:30 -0400

    minor tweaks

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
-from stats import word_count
-from stats import char_count
-from stats import expanded_dic
-from stats import sort_dict
+from stats import (word_count, char_count, expanded_dic, sort_dict)
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -12,21 +10,21 @@ def get_book_text(file_path):
 
 
 def main():
-    try:
-        filepath = sys.argv[1]
-        print("============ BOOKBOT ============")
-        print(f"Analyzing book found at {filepath}")
-        print("----------- Word Count ----------")
-        word_count(filepath)
-        print("--------- Character Count -------")
-        count = expanded_dic(char_count(filepath))
-        count.sort(reverse=True, key=sort_dict)
-        for c in count:
-            letter, num = c["char"], c["num"]
-            if letter.isalpha() == True:
-                print(f"{letter}: {num}")
-        print("============= END ===============")
-    except:
+    if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
+    filepath = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    word_count(filepath)
+    print("--------- Character Count -------")
+    count = expanded_dic(char_count(filepath))
+    count.sort(reverse=True, key=sort_dict)
+    for c in count:
+        letter, num = c["char"], c["num"]
+        if letter.isalpha() == True:
+            print(f"{letter}: {num}")
+    print("============= END ===============")
+        
 main()
