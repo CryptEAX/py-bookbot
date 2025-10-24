12fc6e224a660ba6e644b9c244be84a012eb08b0
CryptEAX <crypteax@proton.me>
2025-10-24 11:37:21 -0400

    Created character count function

--- stats.py
+++ stats.py
@@ -1,5 +1,30 @@
 def word_count(file_path):
     with open(file_path) as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        print(f"Found {len(words)} total words")
+        text = f.read()
+        print(f"Found {len(text.split())} total words")
+
+# def char_count(file_path):
+#     with open(file_path) as f:
+#         text = f.read()
+#         words = text.split()
+#         char_list = {}
+#         for word in words:
+#             for letter in word:
+#                 if letter in char_list:
+#                     char_list[letter.lower()] += 1
+#                 else:
+#                     char_list[letter.lower()] = 1
+#         print(char_list)
+
+
+
+def char_count(file_path):
+    with open(file_path) as f:
+        text = f.read()
+        char_list = {}
+        for letter in text:
+            if letter.lower() in char_list:
+                char_list[letter.lower()] += 1
+            else:
+                char_list[letter.lower()] = 1
+        print(char_list)
